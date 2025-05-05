427d112f596a8838b628943d0c53cd8a1b85cbae
Siva Srinivasa Rao Mothukuri <sivagtr@gmail.com>
2025-05-05 11:10:49 +0530

    Basic Hello World

---
+++ 01-Hello-World/01-HelloWorld.go
@@ -0,0 +1,46 @@
+// Package declaration (Mandatory)
+package main
+
+// Import dependencies
+
+// Package level scope variables/constants/type declarations
+
+// Main function (Entry point of the program)
+func main() {
+	// Print "Hello, World!" to the console
+	println("Hello, World!")
+}
+
+// Other functions (if any)
+
+
+
+
+
+
+
+/* 
+
+To Build and Run the Program
+-----------------------
+go build 01-HelloWorld.go
+
+or
+
+go build -o [binary-file-name] [file-name]
+
+For example, if you want to name the binary file `01-HelloWorld`, you can use the following command:
+go build -o 01-HelloWorld 01-HelloWorld.go
+
+This will create an executable file named `01-HelloWorld` (or `01-HelloWorld.exe` on Windows) in the same directory.
+To run the program, you can execute the following command in your terminal:
+
+./01-HelloWorld
+
+Or
+
+go run 01-HelloWorld.go
+This will compile and run the program in one step.
+
+
+*/
